26b5795a55b8280a49a4a5e3bc65dce3241a66d2
Jaison William Tiu <jater12@berkeley.edu>
2018-07-04 18:01:16 -0700

    update readme conver to functional component

--- src/SearchButton.js
+++ src/SearchButton.js
@@ -1,13 +1,14 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
-class SearchButton extends Component {
-	render() {
-		return(
+
+function SearchButton(props) {
+
+	return(
 				<div className="open-search">
 		              <Link to="/search"/>
 		        </div>
-			)
-	}
+		)
+
 }
 
 export default SearchButton
